e22b3d7a075f2f04bcf82e626fecac0a2e2b7ba9
Frank Peter Schultze <fpschultze@live.de>
2016-12-23 17:13:01 +0100

    First public release

---
+++ Get-CertThumbPrint.ps1
@@ -0,0 +1,34 @@
+﻿function Get-CertThumbprint
+{
+  <#
+  .SYNOPSIS
+   Get the hash value for a .cer file.
+
+  .DESCRIPTION
+   Returns the hexadecimal string representation of a X.509 certificate hash value.
+
+  .EXAMPLE
+   Get-CertThumbprint -CertFile webserver.cer
+
+  .OUTPUTS
+   System.String
+  #>
+
+  Param
+  (
+    # The path to the certificate
+    [Parameter(Mandatory=$true, ValueFromPipeline=$true)]
+    [string]$CertFile
+  )
+  $ErrorActionPreference = 'Stop'
+  try
+  {
+    Write-Verbose 'Load the certificate into an X509Certificate object'
+    $myCert = New-Object -TypeName System.Security.Cryptography.X509Certificates.X509Certificate -ArgumentList $CertFile
+    $myCert.GetCertHashString()
+  }
+  catch
+  {
+    $_.Exception.Message | Write-Error
+  }
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# ConvertTo-DateTime PowerShell Function
+
+SYNOPSIS: Get the hash value for a .cer file.
+
+DESCRIPTION: Returns the hexadecimal string representation of a X.509 certificate hash value.
+
+EXAMPLE: Get-CertThumbprint -CertFile webserver.cer
